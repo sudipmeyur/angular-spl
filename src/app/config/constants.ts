53f5457db6225e999fd1c4600629856362d69849
Sudipmeyur <meyur.sudip@gmail.com>
2026-02-02 12:42:34 +0530

    profile dev

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,4 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
+import { environment } from '../../environments/environment';
+
+export const API_BASE_URL = environment.apiBaseUrl || 'http://localhost:8080/api';
 export const PLAYERS_URL = `${API_BASE_URL}/players`;
 export const PLAYER_TEAMS_URL = `${API_BASE_URL}/player-teams`;
 export const TEAM_SEASONS_URL = `${API_BASE_URL}/team-seasons`;
